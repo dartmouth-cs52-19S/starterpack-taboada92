4836785a59f0093b420e5375d8653bb3031cf37e
Ricardo Taboada <taboada.19@dartmouth.edu>
2019-04-11 13:37:16 -0400

    Deployed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Yes");
+import $ from 'jquery';
+import './style.scss';
+
+
+// References https://codeburst.io/javascript-arrow-functions-for-beginners-926947fc0cdc
+function CountSeconds() {
+  this.seconds = 0;
+  this.counter = setInterval(() => {
+    this.seconds += 1;
+    const message = `You've been on this page for ${this.seconds} seconds`;
+    $('#main').html(message);
+  }, 1000);
+}
+
+const counting = new CountSeconds();
+
+console.log(counting);
